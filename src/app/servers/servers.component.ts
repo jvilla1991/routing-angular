d2986f818fc23837fb0270b98a2ce7455c7fb925
jvilla1991 <villajeremy1991@gmail.com>
2024-06-27 00:13:47 -0400

    450 Navigated relative routes programatically

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,18 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, 
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onRelod() {
+    this.router.navigate( // Unlike the router link, the navigate method does not know what route we're currently on. The routerlink sits in the components template and always knows where it is.
+      ['servers'], 
+    {relativeTo: this.route}); // This will navigate to the specified route, then resolve any routes that we have defined in the array above
   }
 
 }
