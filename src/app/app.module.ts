ad4115344b16c374c2682878caed0885e3a8f78e
jvilla1991 <villajeremy1991@gmail.com>
2024-07-01 23:59:38 -0400

    455 Passing Query Parameters and Fragments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,8 @@ const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
   { path: 'users/:id/:name', component: UserComponent }, // the colon tells Angular that this is a dynamic route
-  { path: 'servers', component: ServersComponent }  
+  { path: 'servers', component: ServersComponent },
+  { path: 'servers/:id/edit', component: EditServerComponent }  
 ]
 
 @NgModule({
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,9 +13,9 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
+  onLoadServers(id: number) {
     // complex calculation that allows us to navigate away... we inject the router above in the constructor
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
   }
 
 }
