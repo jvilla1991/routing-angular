a009145d53e04b3f0594c66dd5880666896eeee9
jvilla1991 <villajeremy1991@gmail.com>
2024-07-06 17:52:09 -0400

    457 Practing and common gotchas(sending in ID as a string)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ const appRoutes: Routes = [
   { path: 'users', component: UsersComponent },
   { path: 'users/:id/:name', component: UserComponent }, // the colon tells Angular that this is a dynamic route
   { path: 'servers', component: ServersComponent },
+  { path: 'servers/:id', component: ServerComponent },
   { path: 'servers/:id/edit', component: EditServerComponent }  
 ]
 
